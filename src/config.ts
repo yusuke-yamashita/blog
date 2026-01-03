9f8b7b1c05ca423cbf0611adc02ea2bbbf5d464f
山下　友輔 <38856833+yusuke-yamashita@users.noreply.github.com>
2026-01-03 10:55:23 +0900

    初コミット

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://55ymzn.com/", // replace this with your deployed domain
+  author: "やまずん",
+  profile: "https://55ymzn.com/",
+  desc: "System Fixer/バキバキQA/Dirty Testerやまずんのサイトです。",
+  title: "やまずん",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
@@ -12,12 +12,12 @@ export const SITE = {
   showArchives: true,
   showBackButton: true, // show back button in post detail
   editPost: {
-    enabled: true,
+    enabled: false,
     text: "Edit page",
     url: "https://github.com/satnaing/astro-paper/edit/main/",
   },
   dynamicOgImage: true,
   dir: "ltr", // "rtl" | "auto"
-  lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  lang: "ja", // html lang code. Set this empty and default will be "en"
+  timezone: "Asia/Tokyo", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
---
+++ src/data/blog/astro_test.md
@@ -0,0 +1,12 @@
+---
+author: 55_ymzn
+title: Astroのテスト
+slug: test
+featured: true
+draft: false
+tags:
+  - テスト
+description: 使えるかどうかのテスト
+---
+
+デプロイしてみよう
